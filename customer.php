45bcb5606dc2a202d31a48ce7cd403cc51a5bc5a
SoulsoftUrmila <127095232+SoulsoftUrmila@users.noreply.github.com>
2023-07-08 18:12:19 +0530

    Urmila Committed by

---
+++ customer.php
@@ -0,0 +1,52 @@
+<div class="customer-reviews-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Customer Reviews</h2>
+						<p>All our dishes will satisfy the connoisseur you have been hiding in the closet.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-8 mr-auto ml-auto text-center">
+					<div id="reviews" class="carousel slide" data-ride="carousel">
+						<div class="carousel-inner mt-4">
+							<div class="carousel-item text-center active">
+								<div class="img-box p-1 border rounded-square m-auto">
+									<img class="d-block w-100 rounded-square" src="" alt="">
+								</div>
+								<h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">Customer 1</strong></h5>
+								<h6 class="text-dark m-0">Best Customer</h6>
+								<p class="m-0 pt-3">This Hotel, in spite of being a Holiday Inn Express Property (no great frills automatically), does a lot more than that, largely because of a very good, guest oriented management. All Teams are very helpful and considerate. Rooms are well maintained and everything that you need for a short stay is provided for or can be requested. Breakfast has enough variety and is well made.My Stay was rather enjoyable, thank You.</p>
+							</div>
+							<div class="carousel-item text-center">
+								<div class="img-box p-1 border rounded-square m-auto">
+									<img class="d-block w-100 rounded-square" src="" alt="">
+								</div>
+								<h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">Customer 2</strong></h5>
+								<h6 class="text-dark m-0">Best Customer</h6>
+								<p class="m-0 pt-3">The staff was really kind. Good hospitality offered ❤️ Breakfast is nice. Over all a nice experience and It was a pleasant stay. Will surely recommend other. Looking forward to spend another vacation at your property. Can come with family and friends or colleagues.</p>
+							</div>
+							<div class="carousel-item text-center">
+								<div class="img-box p-1 border rounded-square m-auto">
+									<img class="d-block w-100 rounded-square" src="" alt="">
+								</div>
+								<h5 class="mt-4 mb-0"><strong class="text-warning text-uppercase">Customer 3</strong></h5>
+								<h6 class="text-dark m-0">Bast Customer</h6>
+								<p class="m-0 pt-3">Room was cozy and super comfy. Staffs were very friendly and cooperative. Food was very nice. I would definitely recommend a stay here. Location wise also its very near to the ahmednagar  . Overall its very nice..</p>
+							</div>
+						</div>
+						<a class="carousel-control-prev" href="#reviews" role="button" data-slide="prev">
+							<i class="fa fa-angle-left" aria-hidden="true"></i>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#reviews" role="button" data-slide="next">
+							<i class="fa fa-angle-right" aria-hidden="true"></i>
+							<span class="sr-only">Next</span>
+						</a>
+                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
